f3bf92c0ad042e281699b75c284b03ba7b8bbdec
juliecardinalli <juliecardinalli@berkeley.edu>
2024-05-06 17:20:32 -0500

    2nd week change

--- src/index.js
+++ src/index.js
@@ -7,24 +7,6 @@ var src_default = {
                headers: {'content-type': 'application/json'},
           });
     }
-    if(request.url == "https://www.cloudflareworkers.com/test") {
-           return new Response('Hello worker!', {
-               headers: {
-                   'content-type': 'text/plain',
-               },
-           });
-       }
-       else{
-           return new Response('Error Worker! Wrong URL', {
-               headers: {
-                   'content-type': 'text/plain',
-               },
-           });
-       }
-  }
-};
-export {
-  src_default as default
 };
 
 
