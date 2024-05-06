71499acd6a910c1425e4e00704484c6164c0257b
juliecardinalli <juliecardinalli@berkeley.edu>
2024-05-06 18:36:23 -0500

    second week final

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-// src/index.js
-var src_default = {
-  async fetch(request, env, ctx) {
-    if (request.method == 'POST') {
-          const responseData = { message; 'Recieved a POST request'};
-	  return new Response(JSON.stringify(responseData), {
-               headers: {'content-type': 'application/json'},
-          });
-    }
-};
-
-
-
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -20,5 +7,42 @@ var src_default = {
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+
+var src_default = {
+	async fetch(request, env, ctx) {
+
+		function getRandomInt(max) {
+			return Math.floor(Math.random() * max);
+		};
+
+		const julies_responses = {
+				1: "this is julies first response",
+				2: "2nd respnse"
+				3: "third response is this",
+				4: "this is the final response"
+		};
+
+		if(request.method == "GET") {
+			const n = getRandomInt(4)
+			const value = julies_responses[n]
+
+			return new Response(value, {headers: {
+                   'content-type': 'application/json',
+               },
+           });
+        }
+
+		else {
+			return new Response('Please use a get request', {headers: {
+                   'content-type': 'application/json',
+               },
+           });
+        }
+    }
+};
+
+export {
+	src_defaut as deafult 
+};
 
 
