6b8f041a3aa041abcb2e2e71391fc7c0ea1a8bf6
juliecardinalli <juliecardinalli@berkeley.edu>
2024-05-14 08:25:38 -0700

    bot score changes

--- src/index.js
+++ src/index.js
@@ -11,37 +11,30 @@
 var src_default = {
 	async fetch(request, env, ctx) {
 
-		function getRandomInt(max) {
-			return Math.floor(Math.random() * max);
-		};
-
-		const julies_responses = {
-			1: "this is julies first response",
-			2: "2nd respnse"
-			3: "third response is this",
-			4: "this is the final response"
-		};
-
 		if(request.method == "GET") {
-			const n = getRandomInt(4)
-			const value = julies_responses[n]
 
-			return new Response(value, {
-				headers: {'content-type': 'application/json',
-				},
-			});
+			let originURL = "https://www.jcardinalli.work/";
+			const bot_score = request.cf.botManagement.score;
+
+			if (bot_score < 30) {
+				originURL = "https://httpbin.org/get";
+				let response = await fetch(origin);
+				return response
+			};
+
+			let response = await fetch(origin);
+			return response
+
+        } else {
+            return new Response('Used non-get request', {
+              		headers: {'Content-Type': 'text/plain'},
+              	});
+			}
         }
-        else {
-        	return new Response('Please use a get request', {
-        		headers: {
-        			'content-type': 'application/json',
-               },
-           });
-        }
-    }
-};
-export{
-	src_defaut as deafult 
+    };
+
+export {
+	src_default as default
 };
 
 
